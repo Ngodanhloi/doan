94f8b0ee1b67efeeaf39713f7f9c01ca63835439
Your Name <ng.du2020@gmail.com>
2025-04-11 11:54:27 +0700

    loi

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -12,20 +12,22 @@ return new class extends Migration
      * @return void
      */
     public function up()
-{
-    if (!Schema::hasTable('personal_access_tokens')) {
+    {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            // Giới hạn độ dài của cột `tokenable_type` xuống 191 ký tự
+            $table->string('tokenable_type', 191);
+            $table->unsignedBigInteger('tokenable_id');
             $table->string('name', 50);
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
+
+            // Thêm chỉ mục trên cột `tokenable_type` và `tokenable_id`
+            $table->index(['tokenable_type', 'tokenable_id']);
         });
     }
-}
-
 
     /**
      * Reverse the migrations.
